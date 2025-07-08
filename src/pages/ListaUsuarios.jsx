a2cda2c7a2c0f4bd208287b586813fe58564e831
Flor <mariaflorenciaperezd@gmail.com>
2025-07-08 18:07:06 +0200

    botones de atras y home

--- src/pages/ListaUsuarios.jsx
+++ src/pages/ListaUsuarios.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 import UsuarioList from "../components/UsuarioList";
 function ListaUsuarios() {
    return (
     <div>
-      <h2>Lista de Usuarios</h2>
+      <h2>Lista de Usuarios</h2> 
+        <Link to="/">
+          <button>Home</button>
+        </Link> 
+        <Link to="/usuarios">
+        <button>Atras</button></Link>
       <UsuarioList />
     </div>
   );
