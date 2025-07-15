4aca21865db16979a99299ca1267756d6143c133
Flor <mariaflorenciaperezd@gmail.com>
2025-07-15 15:58:29 +0200

    feat: estilo en la pagina generar cuadrante

--- src/pages/CuadranteGenerar.jsx
+++ src/pages/CuadranteGenerar.jsx
@@ -1,20 +1,13 @@
 import { Link } from 'react-router-dom';
 import GenerarCuadranteForm from '../components/GenerarCuadranteForm';
+import '../css/CuadranteGenerarPage.css';
 
 const GenerarCuadrante = () => {
   return (
-    <div>
-      <Link to='/cuadrantes/config'>
-      <button>Atras</button>
-      </Link>
-
-      <Link to='/'>
-      <button>Home</button>
-      </Link>
-      
-      <Link to="/cuadrantes/mostrar">
-      <button>Ver Cuadrante</button>
-      </Link>
+    <div className='generar-cuadrante-container'>
+      <Link to='/cuadrantes/config' className='boton-atras-superior'>Atras</Link>
+      <Link to='/' className='boton-home-superior'>Home</Link>
+      <Link to='/cuadrantes/mostrar' className='boton-ver-cuadrante'>Ver Cuadrante</Link>
 
       <GenerarCuadranteForm />
     </div>
@@ -22,6 +15,10 @@ const GenerarCuadrante = () => {
 };
 
 export default GenerarCuadrante;
+
+
+
+
 
 
 
