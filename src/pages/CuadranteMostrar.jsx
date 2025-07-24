a28e1954f69b03f1baa9023b02652db30a883f75
Flor <mariaflorenciaperezd@gmail.com>
2025-07-24 16:00:11 +0200

    header en CuadranteMostrar

--- src/pages/CuadranteMostrar.jsx
+++ src/pages/CuadranteMostrar.jsx
@@ -1,19 +1,18 @@
 import { Link } from 'react-router-dom';
 import MostrarCuadrante from "../components/MostrarCuadrante";
-import '../css/CuadranteMostrarPage.css';
+import '../css/CuadranteMostrarPage.css'
 
 
 function CuadranteMostrar() {
   return (
-    <div className='cuadrante-mostrar-container'>
-      <Link to='/cuadrantes/config' className='boton-atras-superior'>Configurar otro cuadrante</Link>
+    <>
+    <div className='botones-header'>
       <Link to="/"><button className='boton-home-superior'>Home</button></Link>
+      <Link to='/cuadrantes/config' className='boton-atras-superior'>Configurar otro cuadrante</Link>
       <Link to="/cuadrantes/guardados"><button className='boton-ver-cuadrante'>Listar Cuadrantes Guardados</button></Link>
-
-
-      <MostrarCuadrante /> 
-
     </div>
+      <MostrarCuadrante /> 
+    </>
   );
 }
 
